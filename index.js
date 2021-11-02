1baef5a03fa7c859f27949bbc733275dd7991044
RicardoRocha150221046 <150221046@estudantes.ips.pt>
2021-11-02 16:55:13 +0000

    Added image

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = new express();
 
-//app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
-    //res.end('Ta bom');
     res.sendFile(__dirname+'/index.html');
 });
 
